6d9eed126306bbc210bb6d9f0f8b2920f617927e
Dave Hards <unstablesun.gmail.com>
2018-06-22 15:21:32 -0700

    init commit

---
+++ Assets/ADL-Plugins/demos/LZ4/Scripts/LZ4testWebGL.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+
+public class LZ4testWebGL : MonoBehaviour
+{
+#if UNITY_WEBGL
+
+	private string myFile = "testLZ4b.png.lz4";
+
+	//an output Buffer for the decompressed lz4 buffer
+	private byte[] outbuffer = null;
+	private Texture2D tex = null;
+
+	#if UNITY_EDITOR
+		byte[] wwb = null;
+	#else
+		private WWW ww2 = null;
+	#endif
+	
+	string path = "";
+
+	private bool downloadDone2;
+
+	private string log = "";
+
+    //log for output of results
+    void plog(string t)
+    {
+        log += t + "\n"; ;
+    }
+
+	void Start(){
+		path = Application.streamingAssetsPath;
+
+		tex = new Texture2D(1600,1280,TextureFormat.RGBA32, false);
+		//get an lz4 file as saved buffer from StreamingAssets
+		#if !UNITY_EDITOR
+			StartCoroutine( getFromStreamingAssets() );		
+		#else
+			StartCoroutine( getFromStreamingAssetsEditor() );
+		#endif
+		
+    }
+
+
+	
+	void OnGUI(){
+		
+		if (downloadDone2 == true) {
+			GUI.Label(new Rect(10, 0, 250, 30), "got package, ready to extract");
+		
+		
+
+			if (GUI.Button(new Rect(10, 90, 230, 50), "start StreamingAssets lz4 test")) {
+				#if !UNITY_EDITOR
+					plog("lz4 decompress: "+LZ4.decompressBuffer(ww2.bytes, ref outbuffer).ToString() );
+				#else
+					plog("lz4 decompress: "+LZ4.decompressBuffer(wwb, ref outbuffer).ToString() );
+				#endif
+					if(outbuffer != null) { tex.LoadImage(outbuffer); }
+
+			}
+		}
+
+		if(tex != null) GUI.DrawTexture(new Rect(360, 10, 375, 300), tex);
+
+		GUI.TextArea(new Rect(10, 370, Screen.width - 20, Screen.height - 400), log);
+				
+	}
+
+
+
+	// ============================================================================================================================================================= 
+
+	
+
+	#if !UNITY_EDITOR
+    IEnumerator getFromStreamingAssets() {
+		plog("getting buffer from StreamingAssets ...");
+
+        ww2 = new WWW(path +"/"+ myFile);
+
+        yield return ww2;
+
+        if (ww2.error != null) plog("Streaming Assets Error: "+ww2.error.ToString());
+		else outbuffer = new byte[ 0 ];
+		plog("Got buffer");
+		downloadDone2 = true;
+    }
+	#else
+
+	IEnumerator getFromStreamingAssetsEditor() {
+		plog("getting buffer from StreamingAssets ...");
+		yield return true;
+		
+		if(System.IO.File.Exists(path+"/"+myFile)) {
+			wwb = System.IO.File.ReadAllBytes(path+"/"+myFile);
+		}
+		if(wwb == null) plog("Could not find file: " + myFile + " in StreamingAssets");
+		else outbuffer = new byte[ 0 ];
+		plog("Got buffer");
+		downloadDone2 = true;
+	}
+	#endif
+#else
+    void Start(){
+        Debug.Log("Only for WebGL");
+    }
+#endif
+
+}
+
---
+++ Assets/Scripts/LoadFileManager.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+using System.Threading;
+
+public class LoadFileManager : MonoBehaviour 
+{
+
+    //we use some integer to get error codes from the lzma library (look at lzma.cs for the meaning of these error codes)
+    private int zres = 0;
+
+    private string myFile;
+    private WWW www;
+
+    private string log;
+
+    private string ppath;
+
+    private bool compressionStarted, pass;
+    private bool downloadDone;
+
+    //reusable buffers
+    private byte[] reusableBuffer, reusableBuffer2, reusableBuffer3;
+
+    //fixed size buffers, that don't get resized, to perform compression/decompression of buffers in them and avoid memory allocations.
+    private byte[] fixedInBuffer = new byte[1024 * 256];
+    private byte[] fixedOutBuffer = new byte[1024 * 768];
+    private byte[] fixedBuffer = new byte[1024 * 1024];
+
+    //A single item integer array that changes to the current number of file that get uncompressed of a zip archive.
+    //When running the decompress_File function, compare this int to the total number of files returned by the getTotalFiles function
+    //to get the progress of the extraction if the zip contains multiple files.
+    //If you use multiple threads, remember to use other progress integers for the other threads, otherwise there will be a sharing violation.
+    //
+    private int[] progress = new int[1];
+
+    //individual file progress (in bytes)
+    private int[] progress2 = new int[1];
+
+
+
+	void Start () 
+    {
+        ppath = Application.temporaryCachePath;
+        Debug.Log(ppath);
+
+        //various byte buffers for testing
+        reusableBuffer = new byte[4096];
+        reusableBuffer2 = new byte[0];
+        reusableBuffer3 = new byte[0];
+
+        myFile = "challenge-bundle.zip";
+
+
+        PrintTemporaryCachePath();
+
+	}
+	
+	void Update () 
+    {
+		
+	}
+
+
+    public void onFileLoadButtonClicked()
+    {
+        
+        Debug.Log("onFileLoadButtonClicked");
+
+        StartCoroutine(DownloadFile());
+    }
+
+    public void onExpandFileButtonClicked()
+    {
+
+        Debug.Log("onExpandFileButtonClicked");
+
+
+        DoDecompression();
+
+    }
+
+
+    private void PrintPersistentDataPath()
+    {
+        Debug.Log("persistentDataPath" + Application.persistentDataPath);
+    }
+
+    private void PrintTemporaryCachePath()
+    {
+        Debug.Log("temporaryCachePath" + Application.temporaryCachePath);
+    }
+
+
+
+    IEnumerator DownloadFile()
+    {
+        var uwr = new UnityWebRequest("https://s3.amazonaws.com/master-chef-debug/asset-bundle-zips/challenge-bundle.zip", UnityWebRequest.kHttpVerbGET);
+        //string path = Path.Combine(Application.persistentDataPath, "challenge-bundle.zip");
+        string path = Path.Combine(Application.temporaryCachePath, "challenge-bundle.zip");
+        uwr.downloadHandler = new DownloadHandlerFile(path);
+        yield return uwr.SendWebRequest();
+        if (uwr.isNetworkError || uwr.isHttpError)
+            Debug.LogError(uwr.error);
+        else
+            Debug.Log("File successfully downloaded and saved to " + path);
+    }
+
+
+
+
+    void DoDecompression()
+    {
+
+
+
+        //validate sanity of a zip archive
+        Debug.Log("Validate: " + lzip.validateFile(ppath + "/challenge-bundle.zip").ToString());
+
+
+        //decompress the downloaded file
+        zres = lzip.decompress_File(ppath + "/challenge-bundle.zip", ppath + "/", progress, null, progress2);
+        Debug.Log("decompress: " + zres.ToString());
+        Debug.Log("");
+
+        //get the true total files of the zip
+        Debug.Log("true total files: " + lzip.getTotalFiles(ppath + "/challenge-bundle.zip"));
+
+    }
+
+
+
+
+
+
+}
